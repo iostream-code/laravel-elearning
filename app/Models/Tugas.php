9ef795d08a94ba8cfcc04a05f236d4b06d25e3e1
iostream-code <aryakmifjannah2603@gmail.com>
2023-12-04 17:34:07 +0700

    changing name

--- app/Models/Tugas.php
+++ app/Models/Tugas.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Tugas extends Model
 {
     use HasFactory;
+
+    protected $fillabe = [
+        'judul',
+        'isi',
+        'deksripsi',
+    ];
 }
